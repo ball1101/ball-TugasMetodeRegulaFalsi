658b4601b17faa82372ec0fcd84c904db8ab4257
ball1101 <iqballlms11@gmail.com>
2026-01-03 18:05:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new")
+st.title("🎈new")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
